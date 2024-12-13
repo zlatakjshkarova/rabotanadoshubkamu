d3d2fd9c84c590e8b49cd598cf93976fefe5d25d
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-12-13 21:16:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+
+//5 задание
+let arr1 = ['a', 'b', 'c', 'd'];
+console.log(arr1)
+console.log(arr1[arr1.length - 1]);
+
+
+//задание 6
+let af = ['a', 'b', 'c'];
+let se = af.includes(1)
+let se1 = af.includes(2)
+let se2 = af.includes(3)
+console.log(se, se1, se2)
+
+//задание 7
+let numbers = [1, 2, 3];
+let njd = numbers = [1 + 3, 2 + 3, 3 + 3];
+console.log(njd)
+//8
+let numbers3 = [];
+for (let n = 1; n <= 20; n++) {
+    numbers3.push(n)
+}
+console.log(numbers3)
+
+
+
+// 9 задание 
+let numbers4 = [];
+for (let s = 10; s <= 40; s++) {
+    numbers4.push(s)
+}
+console.log(numbers4)
+//10 задание 
+
+// не знаю как делать 
+
+//11 задание 
+let numfod = [];
+for (let f = -10; f <= 0; f++) {
+    numfod.push(f)
+}
+console.log(numfod)
+//12 задание 
+//не знаю как делать
+
+//13 задание 
+let arwr = [1, 2, 3, 4];
+let f = arwr.includes(3);
+console.log(f)
+
+//14 задание 
+let arr = []
+for (let s = 1; s <= 3; s++) {
+    arr.push(s)
+}
+console.log(arr)
